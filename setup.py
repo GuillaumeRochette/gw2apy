f19f5b9821e51b3f53dae09fdc141543feda1454
Rochette, Guillaume L (PG/R - Elec Electronic Eng) <g.rochette@surrey.ac.uk>
2021-03-10 12:30:19 +0000

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6"
-        "Programming Language :: Python :: 3.7"
-        "Programming Language :: Python :: 3.8"
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "Natural Language :: English",
     ],
